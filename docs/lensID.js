8039db79b5abeb57e76f1d66784d6085f5a06f8f
Bastien Sarolea <sarobastien@gmail.com>
2025-05-16 14:39:35 +0200

    02

--- docs/lensID.js
+++ docs/lensID.js
@@ -1,7 +1,7 @@
 window.lensID = 'b8e614e9-bd59-429b-94a8-05ae9385210a'; //Third Sky Lens;
 window.groupID = 'f7f4e367-f4b3-4de5-8e81-e9c842f2bf0b'; //LIVE_PROD Group;
-window.modePhoto = true;
-window.modeVideo = true;
-window.modeFaceCamera = true;
+window.modePhoto = false;
+window.modeVideo = false;
+window.modeFaceCamera = false;
 window.modeBackCamera = true;
-window.modeStartFaceCamera = true;
+window.modeStartFaceCamera = false;
